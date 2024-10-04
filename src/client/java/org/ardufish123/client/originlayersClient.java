9e107a264b1b52a3c496b0084b41ddb4c8a8701e
IntelliFish123 <vlad.robert.juganaru@gmail.com>
2024-10-04 20:55:24 +0300

    Initial commit

---
+++ src/client/java/org/ardufish123/client/originlayersClient.java
@@ -0,0 +1,10 @@
+package org.ardufish123.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class originlayersClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/ardufish123/originlayers.java
@@ -0,0 +1,10 @@
+package org.ardufish123;
+
+import net.fabricmc.api.ModInitializer;
+
+public class originlayers implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
